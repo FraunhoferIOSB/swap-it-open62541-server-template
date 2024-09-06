fbcada9cf29af145de8d630481425799f3e3415c
FlorianDue <flo@KIS-flo47663-1.pc.iitb.fhg.de>
2024-09-06 13:46:50 +0200

    feat(open62541-server-template): add argument description for utility functions

--- deps/template/include/swap_it.h
+++ deps/template/include/swap_it.h
@@ -17,15 +17,17 @@
 
 /*
  * Method Arguments:
- * UA_Server *server:               the server instance
- * UA_MethodCallback callback:      callback of for the service method
- * UA_Boolean default_behavior:     boolean that determines wheter a default information
- *                                  model will be instantiated based on the json configuration
- * UA_Boolean *running:             running variable from the server's main function
- * UA_Boolean register_agent:       Boolean that determines wheter the Asset should register itself in a device registry
- * UA_String registry_url:          URL of the Device Regsitry instance the Asset should register itself in.
- *                                  Argument has the form ip:port => 0.0.0.0:4840
- * */
+ * UA_Server *server:                           the server instance
+ * UA_ByteString json                           json configuration of the server
+ * UA_MethodCallback callback:                  callback of for the service method
+ * UA_Boolean default_behavior:                 boolean that determines whether a default information
+ *                                              model will be instantiated based on the json configuration
+ * UA_Boolean *running:                         running variable from the server's main function
+ * UA_Boolean register_agent_in_registry:       Boolean that determines whether the Asset should register itself in a device registry
+ * UA_service_server_interpreter *swap_server   Empty structure that stores the interpreted json configuration. The stucture is filled
+ *                                              when interpreting the json config within the function and can then be used for the function
+ *                                              clear_swap_server
+*/
 
 UA_StatusCode UA_server_swap_it(UA_Server *server,
                                 UA_ByteString json,
@@ -34,6 +36,13 @@ UA_StatusCode UA_server_swap_it(UA_Server *server,
                                 UA_Boolean *running,
                                 UA_Boolean register_agent_in_registry,
                                 UA_service_server_interpreter *swap_server);
+
+/*
+ * UA_service_server_interpreter *server_info       Filled structure from the function UA_server_swap_it
+ * UA_Boolean unregister                            Boolean value that determines whether the server should
+ *                                                  unregister itself from the device registry
+ * UA_Server *server                                the server instance
+*/
 
 void clear_swap_server(UA_service_server_interpreter *server_info, UA_Boolean unregister, UA_Server *server);
 
